47e54d721b467c4e7f753175088a188bc815b669
nopalllfd <Naufalfadhilah689@gmail.com>
2026-05-18 10:16:47 +0700

    feat: add cors middleware

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -1,11 +1,14 @@
 package router
 
 import (
+	"backend-golang/internal/middleware"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func InitRoutes(app *gin.Engine, db *pgxpool.Pool) {
+	app.Use(middleware.CORSMiddleware)
 	api := app.Group("/api")
 	{
 		SetupAuthRoute(api, app, db)
